f087d669743b06edf6399baf085f5ecbcd5b0774
Rick Sun <yatyricky@gmail.com>
2026-05-03 00:06:34 +0800

    clear sample project

--- samples/CSProject/Hero.cs
+++ samples/CSProject/Hero.cs
@@ -2,12 +2,20 @@ namespace Game;
 
 public class Hero : Unit
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="hp"></param>
     public Hero(string name, double hp) : base(name, hp)
     {
         Name = "H" + name;
         HP = hp * 2;
     }
 
+    //
+    /**
+    */
     public override void LevelUp()
     {
         base.LevelUp();
